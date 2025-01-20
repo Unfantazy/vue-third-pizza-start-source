45d65241d518569a2e1e2864b33be11bccf2e9e9
unfantazy <akuptsova@korusconsulting.ru>
2025-01-20 12:54:59 +0300

    section 1 (v-for)

---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <app-header />
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import AppHeader from "./AppHeader.vue";
+</script>
+
+<style scoped lang="scss"></style>
